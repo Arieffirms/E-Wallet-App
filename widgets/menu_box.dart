7f327e73dcecaa2c8e6c7fae8dd79a95242b1343
Arieffirms <arieffirmansyah0731@gmail.com>
2024-02-08 23:55:34 +0700

    melanjutkan menu_box

--- widgets/menu_box.dart
+++ widgets/menu_box.dart
@@ -1,10 +1,31 @@
 import 'package:flutter/material.dart';
 
 class MenuBox extends StatelessWidget {
-  const MenuBox({super.key});
+  const MenuBox({super.key, required this.icon, required this.text});
+  final IconData icon;
+  final String text;
 
   @override
   Widget build(BuildContext context) {
-    return ;
+    return Padding(
+      padding: EdgeInsets.symmetric(
+        horizontal: 10,
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            width: 65,
+            height: 65,
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+              color: Colors.blue.shade300,
+              borderRadius: BorderRadius.circular(20),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
